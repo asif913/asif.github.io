430c381a04d0991ac89ca8f9f28a9ae76b1ea5e1
asif913 <75655616+asif913@users.noreply.github.com>
2022-06-09 22:49:19 +0600

    Update index.js

--- index.js
+++ index.js
@@ -34,15 +34,9 @@ $(document).ready(function(){
     });
 
     // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
+   
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
